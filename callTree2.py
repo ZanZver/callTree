7f3e3e8494f22f3a2711fef264c9458e096c92fc
Zan Zver <zan.zver98@gmail.com>
2019-04-05 21:19:24 +0100

    2 versions of call tree

---
+++ callTree2.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self, value, children=None):
+        # a leaf node has an empty list of children
+        if children is None:
+            children = []
+        self.value = value
+        self.children = children
+
+root = Node(1, [
+    Node(2, [
+        Node(3, [
+            Node(4),
+            Node(5)
+        ]),
+        Node(6)
+    ]),
+    Node(7, [
+        Node(8, [
+            Node(9)
+        ])
+    ]),
+    Node(10)
+])
+
+def tree_sum(node):
+    print('Called with node value', node.value)
+    t = node.value
+    for child in node.children:
+        t += tree_sum(child)
+    print('Returned', t)
+    return t
+
